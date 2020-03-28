6a7341b8055e56e5ce783f3aa078238df5189d5b
dildeolupbiten <tckutlu@gmail.com>
2020-03-28 15:59:47 +0300

    Removed blank line.

--- Scripts/constants.py
+++ Scripts/constants.py
@@ -3,7 +3,6 @@
 CONFIRMED = "time_series_covid19_confirmed_global.csv"
 DEATHS = "time_series_covid19_deaths_global.csv"
 RECOVERED = "time_series_covid19_recovered_global.csv"
-
 URL = "https://raw.githubusercontent.com/CSSEGISandData/"\
     "COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
 URL_CONFIRMED = URL + CONFIRMED
