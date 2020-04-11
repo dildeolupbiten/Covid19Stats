351a34e8db73fee50828e2040e3828a83e3a1745
dildeolupbiten <tckutlu@gmail.com>
2020-04-11 16:46:45 +0300

    Increase rate of the proportions along a timeline will be shown anymore.

--- Scripts/menu.py
+++ Scripts/menu.py
@@ -13,6 +13,14 @@ class Menu:
         self.view = tk.Menu(master=self.master, tearoff=False)
         self.proportion = tk.Menu(master=self.master, tearoff=False)
         self.increase_rate = tk.Menu(master=self.master, tearoff=False)
+        self.increase_rate_cases = tk.Menu(
+            master=self.master,
+            tearoff=False
+        )
+        self.increase_rate_proportions = tk.Menu(
+            master=self.master,
+            tearoff=False
+        )
         self.compare = tk.Menu(master=self.master, tearoff=False)
         self.compare_proportion = tk.Menu(master=self.master, tearoff=False)
         self.master.add_cascade(
@@ -34,6 +42,14 @@ class Menu:
         self.master.add_cascade(
             label="Compare Proportion Graphs",
             menu=self.compare_proportion
+        )
+        self.increase_rate.add_cascade(
+            label="Case Rates",
+            menu=self.increase_rate_cases
+        )
+        self.increase_rate.add_cascade(
+            label="Proportion Rates",
+            menu=self.increase_rate_proportions
         )
         self.view.add_command(
             label="Confirmed",
@@ -92,7 +108,7 @@ class Menu:
                 select="proportion"
             )
         )
-        self.increase_rate.add_command(
+        self.increase_rate_cases.add_command(
             label="Confirmed",
             command=lambda: self.view_data(
                 data=self.treeview.confirmed_data,
@@ -100,7 +116,7 @@ class Menu:
                 select="increase_rate"
             )
         )
-        self.increase_rate.add_command(
+        self.increase_rate_cases.add_command(
             label="Deaths",
             command=lambda: self.view_data(
                 data=self.treeview.deaths_data,
@@ -108,12 +124,45 @@ class Menu:
                 select="increase_rate"
             )
         )
-        self.increase_rate.add_command(
+        self.increase_rate_cases.add_command(
             label="Recovered",
             command=lambda: self.view_data(
                 data=self.treeview.recovered_data,
                 title="Recovered",
                 select="increase_rate"
+            )
+        )
+        self.increase_rate_proportions.add_command(
+            label="Deaths/Confirmed",
+            command=lambda: self.view_data(
+                data=[
+                    self.treeview.confirmed_data,
+                    self.treeview.deaths_data
+                ],
+                title="Deaths/Confirmed",
+                select="increase_rate_proportion"
+            )
+        )
+        self.increase_rate_proportions.add_command(
+            label="Recovered/Confirmed",
+            command=lambda: self.view_data(
+                data=[
+                    self.treeview.confirmed_data,
+                    self.treeview.recovered_data
+                ],
+                title="Recovered/Confirmed",
+                select="increase_rate_proportion"
+            )
+        )
+        self.increase_rate_proportions.add_command(
+            label="Recovered/Deaths",
+            command=lambda: self.view_data(
+                data=[
+                    self.treeview.deaths_data,
+                    self.treeview.recovered_data
+                ],
+                title="Recovered/Deaths",
+                select="increase_rate_proportion"
             )
         )
         self.compare.add_command(
@@ -175,7 +224,7 @@ class Menu:
         )
         
     @staticmethod
-    def find_proportion(x=None, y=None):
+    def find_proportion(x: list = [], y: list = []):
         if not all([x, y]):
             x = []
             y = []
@@ -196,17 +245,28 @@ class Menu:
                 result.append(0)
         return result
     
-    def predict(self, case, increase, day):
+    def predict(
+            self,
+            case: int = 0,
+            day: int = 0,
+            increase: float = .0,
+            proportion: bool = False
+    ):
         if not day:
-            return int(case)
+            if not proportion:
+                return int(case)
+            else:
+                return round(case, 2)
         else:
             return self.predict(
-                case + (case * increase / 100), 
-                increase, 
-                day - 1
+                case=case + (case * increase / 100),
+                day=day - 1,
+                increase=increase,
+                proportion=proportion
             )
-            
-    def deviation_percent(self, observed, expected):
+
+    @staticmethod
+    def deviation_percent(observed: float = .0, expected: float = .0):
         return f"{round((expected - observed) * 100 / expected, 2)} %"
 
     @staticmethod
@@ -233,7 +293,110 @@ class Menu:
                     if item == j[0]:
                         values.append([int(k) for k in j[3:]])
             return values
-              
+
+    def get_values_for_proportion(
+            self, data: list = [],
+            items: list = [],
+            compare: bool = False
+    ):
+        data1, data2 = data[0], data[1]
+        return self.get_values(
+            items=items,
+            data=data1,
+            compare=compare
+        ), self.get_values(
+            items=items,
+            data=data2,
+            compare=compare
+        )
+
+    def create_toplevel(
+            self, title: str = "",
+            countries: str = "",
+            values: list = [],
+            proportion: bool = False
+    ):
+        toplevel = tk.Toplevel()
+        toplevel.title(f"{countries} - {title}")
+        toplevel.resizable(width=False, height=False)
+        frame = tk.Frame(master=toplevel)
+        frame.pack(side="top")
+        columns = (
+            "Date",
+            title,
+            "Increase Percent",
+            "Mean Increase Percent",
+            "Prediction of the next day",
+            "Deviation Percent"
+        )
+        treeview = Treeview(
+            master=frame,
+            columns=columns,
+            csv=False
+        )
+        mean = []
+        all_data = []
+        for ind, (i_, j_) in enumerate(
+                zip(self.treeview.times, values)
+        ):
+            try:
+                increase_rate = 100 \
+                    * (values[ind] - values[ind - 1]) \
+                    / values[ind - 1]
+            except:
+                increase_rate = 0
+            if ind == 0:
+                increase_rate = 0
+            if ind == 0:
+                deviation = "0 %"
+            else:
+                try:
+                    deviation = self.deviation_percent(
+                        observed=values[ind],
+                        expected=self.predict(
+                            case=values[ind - 1],
+                            day=1,
+                            increase=mean[-1],
+                            proportion=proportion
+                        )
+                    )
+                except:
+                    deviation = "0 %"
+            mean.append(increase_rate)
+            if proportion:
+                j_ = round(j_, 2)
+            prediction = self.predict(
+                case=j_,
+                day=1,
+                increase=increase_rate,
+                proportion=proportion
+            )
+            row_data = [
+                i_.strftime('%Y.%m.%d'),
+                j_,
+                f"{round(increase_rate, 2)} %",
+                f"{round(sum(mean) / len(mean), 2)} %",
+                prediction,
+                deviation
+            ]
+            treeview.insert(
+                parent="",
+                index=ind,
+                values=[k for k in row_data]
+            )
+            all_data.append(row_data)
+        countries = countries.replace("=", "-")
+        button = tk.Button(
+            master=toplevel,
+            text="Export",
+            command=lambda: Spreadsheet(
+                filename=f'{countries}_{title.replace("/", "-")}.xlsx',
+                columns=columns,
+                data=all_data
+            )
+        )
+        button.pack(side="bottom")
+
     def view_data(
             self, 
             data=None, 
@@ -251,44 +414,31 @@ class Menu:
                 if len(items) < 6 \
                 else f"Selected Countries = {len(items)}"
             if select == "proportion":
-                data1, data2 = data[0], data[1]
-                values = [[], []]
-                values[0] = self.get_values(
-                    items=items,
-                    data=data1
-                )
-                values[1] = self.get_values(
-                    items=items,
-                    data=data2
+                value1, value2 = self.get_values_for_proportion(
+                    data=data,
+                    items=items
                 )
                 plot_data(
                     x=self.treeview.times, 
                     y=self.find_proportion(
-                        x=values[1],
-                        y=values[0],
+                        x=value2,
+                        y=value1,
                     ),
                     country=countries,
                     title=title
                 )
             elif select == "proportion-compare":
-                data1, data2 = data[0], data[1]
-                values = [[], []]
-                values[0] = self.get_values(
+                value1, value2 = self.get_values_for_proportion(
+                    data=data,
                     items=items,
-                    data=data1,
                     compare=True
                 )
-                values[1] = self.get_values(
-                    items=items,
-                    data=data2,
-                    compare=True
-                )
-                if 2 <= len(values[0]) < 6:
+                if 2 <= len(value1) < 6:
                     plot_data(
                         x=self.treeview.times,
                         y=self.find_proportion(
-                            x=values[1],
-                            y=values[0],
+                            x=value2,
+                            y=value1,
                         ),
                         country=countries,
                         title=title,
@@ -302,74 +452,24 @@ class Menu:
                                 "and at most 5."
                     )
             elif select == "increase_rate":
-                values = self.get_values(items=items, data=data)
-                toplevel = tk.Toplevel()
-                toplevel.title(f"{countries} - {title}")
-                toplevel.resizable(width=False, height=False)
-                frame = tk.Frame(master=toplevel)
-                frame.pack(side="top")
-                columns = (
-                    "Date",
-                    title,
-                    "Increase Percent",
-                    "Mean Increase Percent",
-                    "Prediction of the next day",
-                    "Deviation Percent"
+                values = self.get_values(data=data, items=items)
+                self.create_toplevel(
+                    title=title,
+                    countries=countries,
+                    values=values
                 )
-                treeview = Treeview(
-                    master=frame,
-                    columns=columns,
-                    csv=False
+            elif select == "increase_rate_proportion":
+                y, x = self.get_values_for_proportion(
+                    data=data,
+                    items=items
                 )
-                mean = []
-                all_data = []
-                for ind, (i_, j_) in enumerate(
-                        zip(self.treeview.times, values)
-                ):
-                    try:
-                        increase_rate = 100 \
-                            * (values[ind] - values[ind - 1]) \
-                            / values[ind - 1]
-                    except:
-                        increase_rate = 0
-                    if ind == 0:
-                        increase_rate = 0
-                    if ind == 0:
-                        deviation = "0 %"
-                    else:
-                        try:
-                            deviation = self.deviation_percent(
-                                values[ind], 
-                                self.predict(values[ind - 1], mean[-1], 1)
-                            )
-                        except:
-                            deviation = "0 %"
-                    mean.append(increase_rate)
-                    row_data = [
-                        i_.strftime('%Y.%m.%d'),
-                        j_,
-                        f"{round(increase_rate, 2)} %",
-                        f"{round(sum(mean) / len(mean), 2)} %",
-                        self.predict(j_, increase_rate, 1),
-                        deviation
-                    ]
-                    treeview.insert(
-                        parent="",
-                        index=ind,
-                        values=[k for k in row_data]
-                    )
-                    all_data.append(row_data)
-                countries = countries.replace("=", "-")
-                button = tk.Button(
-                    master=toplevel,
-                    text="Export",
-                    command=lambda: Spreadsheet(
-                        filename=f'{countries}_{title}.xlsx',
-                        columns=columns,
-                        data=all_data
-                    )
+                values = self.find_proportion(x=x, y=y)
+                self.create_toplevel(
+                    title=title,
+                    countries=countries,
+                    values=values,
+                    proportion=True
                 )
-                button.pack(side="bottom")
             elif select == "view":
                 values = self.get_values(items=items, data=data)
                 plot_data(
